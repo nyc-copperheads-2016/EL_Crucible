a7291a7e7661d98ce5a3cae323646a9fc27f3371
Aaron Tsai & Ali Lyons & guillermo neria <pair+aattsai.arlyons.guillermoneira@devbootcamp.com>
2015-11-20 16:39:41 -0500

    Updated Flashcards

--- source/flashcards.rb
+++ source/flashcards.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pry'
 
 class Flashcards
   attr_reader :question, :answer
@@ -10,47 +11,58 @@ class Flashcards
 end
 
 class ArtificialIntelligence
-  def initialize(deck)
+  attr_accessor :incorrect_answer
+
+  def initialize
     @view = View.new
-    @deck = deck
-    check
-
+    @incorrect_answer = []
   end
 
-  def check
-
-    input = ""
-    incorrect_answer = []
-    @view.display_instruction
-    @deck.each do |card|
-      # counter = 0
-      # until counter == @deck.length
-      # counter += 1
-
-      puts card.question
-      input = @view.get_input
-      if input.downcase == card.answer.downcase
-        @view.correct
-
-      else
-        @view.incorrect
-        incorrect_answer << card.question
-      end
-
+  def check(card, guess)
+    # guess = ""
+    if guess.downcase == card.answer.downcase
+      @view.correct
+    else
+      @view.incorrect
+      @incorrect_answer << card.question
     end
-    @view.list_incorrect(incorrect_answer)
+    # sleep 3
   end
-
-
 end
 
 
 class Controller
-  def initialize(args)
+
+  attr_reader :ai, :view
+
+  def initialize(filename)
+    @ai = ArtificialIntelligence.new
+    @view = View.new
+    # view.display_instruction
+    deck_loader(filename)
 
   end
 
+  def deck_loader(deck_choice)
+    deck = Parser.create_deck(deck_choice)
+    run(deck)
+  end
 
+  def run(deck)
+
+    # binding.pry
+    view.display_instruction
+    deck.each do |card|
+      # binding.pry
+      guess =""
+      puts card.question
+      guess = view.get_guess
+      # binding.pry
+      ai.check(card, guess)
+    end
+    view.list_incorrect(ai.incorrect_answer)
+
+  end
 end
 
 class View
@@ -59,29 +71,31 @@ class View
     puts "Please answer each question with true or false."
   end
 
-  # def display_question
-
-  # end
-
   def correct
-    puts "You are correct"
+    puts "You are correct!"
+    question_divider
   end
 
    def incorrect
     puts "Sorry you are incorrect :( "
+    question_divider
   end
 
-  def get_input
-    gets.chomp
+  def get_guess
+    STDIN.gets.chomp
   end
 
   def list_incorrect(incorrect_answer)
+    question_divider
     puts "Here is the list of incorrect responses: "
     incorrect_answer.each do |incorrect|
       puts incorrect
     end
   end
 
+  def question_divider
+    puts "========================================"
+  end
 end
 
 
@@ -91,7 +105,7 @@ module Parser
     CSV.foreach(file, :headers => true, :header_converters => :symbol).each do |row|
      deck << Flashcards.new(row)
      end
-     ArtificialIntelligence.new(deck)
+     deck
   end
 end
 
@@ -99,9 +113,13 @@ end
 
 
 
-test = Parser.create_deck('deck.csv')
+# test = Parser.create_deck('deck.csv')
+# control = Controller.new(Parser.create_deck('deck.csv'))
+# control
+# # first_card = test.each { |card| p card.answer}
+# if ARGV[0] == "1"
 
-test
-# first_card = test.each { |card| p card.answer}
+test = Controller.new(ARGV[0])
+
 
 
